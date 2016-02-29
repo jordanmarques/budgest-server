fef414a974ebef0720ca0fcb754ebdb288830e89
Jordan Marques <jordan.marques@vidal.fr>
2016-02-29 09:19:19 +0100

    start rest API implementation

---
+++ src/main/java/controller/PersonController.java
@@ -0,0 +1,17 @@
+package controller;
+
+import pojo.Person;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PersonController {
+
+    @RequestMapping("/person")
+    public Person person(@RequestParam(value="firstName") String firstName,
+                         @RequestParam(value="lastName") String lastName) {
+
+        return new Person(1, firstName, lastName);
+    }
+}
