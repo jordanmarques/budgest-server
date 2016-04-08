d96c5019f5ee5be3527d2c4bbc4385aa72cad519
Karthi <karthikan293@gmail.com>
2016-04-08 20:27:41 +0200

    - Simple view to see persons

--- src/main/resources/static/scripts/app.js
+++ src/main/resources/static/scripts/app.js
@@ -9,42 +9,37 @@
  * Main module of the application.
  */
 angular
-  .module('budGestApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/persons', {
-        templateUrl: 'views/persons.html',
-        controller: 'PersonsCtrl',
-        controllerAs: 'persons'
-      })
-      .when('/budgests', {
-        templateUrl: 'views/budgests.html',
-        controller: 'BudgestsCtrl',
-        controllerAs: 'budgests'
-      })
-      .when('/budgests', {
-        templateUrl: 'views/budgests.html',
-        controller: 'BudgestsCtrl',
-        controllerAs: 'budgests'
-      })
-      .when('/events', {
-        templateUrl: 'views/events.html',
-        controller: 'EventsCtrl',
-        controllerAs: 'events'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+    .module('budGestApp', [
+        'ngAnimate',
+        'ngCookies',
+        'ngResource',
+        'ngRoute',
+        'ngSanitize',
+        'ngTouch'
+    ])
+    .config(function ($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'views/main.html',
+                controller: 'MainCtrl',
+                controllerAs: 'main'
+            })
+            .when('/persons', {
+                templateUrl: 'views/persons.html',
+                controller: 'PersonsCtrl',
+                controllerAs: 'persons'
+            })
+            .when('/budgests', {
+                templateUrl: 'views/budgests.html',
+                controller: 'BudgestsCtrl',
+                controllerAs: 'budgests'
+            })
+            .when('/events', {
+                templateUrl: 'views/events.html',
+                controller: 'EventsCtrl',
+                controllerAs: 'events'
+            })
+            .otherwise({
+                redirectTo: '/'
+            });
+    });
