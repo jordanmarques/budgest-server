a021ccf2fb40708cad3b80c4a9e9eb90d74442ee
salfai <salfai.fc@gmail.com>
2016-04-11 15:06:30 +0200

    - add a budget set angular form

--- src/main/resources/static/scripts/controllers/budgests.js
+++ src/main/resources/static/scripts/controllers/budgests.js
@@ -10,18 +10,18 @@
 angular.module('budGestApp')
     .controller('BudgestsCtrl', function ($scope, $http) {
         /*this.awesomeThings = [
-            'HTML5 Boilerplate',
-            'AngularJS',
-            'Karma'
-        ];*/
+         'HTML5 Boilerplate',
+         'AngularJS',
+         'Karma'
+         ];*/
 
         //$scope.budgets = [{'@jsonId':1,'id':1,'global_amount':250.0,'event':'JOJOJOJOJO','category':'JOJOJOJO','manager':{'@jsonId':2,'personId':1,'firstName':'Jonathan','lastName':'Joestar','budgets':[1]}},{'@jsonId':3,'id':2,'global_amount':15656.8,'event':'acheter un arbre','category':'JOJOJOJOJO','manager':{'@jsonId':4,'personId':2,'firstName':'Joseph','lastName':'Joestar','budgets':[3]}}];
 
 
         /*$http.get('budget').success(function (data) {
 
-            $scope.budgets = data;
-        });*/
+         $scope.budgets = data;
+         });*/
 
         $http.get('budget')
             .success(function (data) {
@@ -36,3 +36,36 @@ angular.module('budGestApp')
             });
 
     });
+
+
+angular.module('budGestApp')
+    .controller('addBudget', function ($scope, $http) {
+        /*var user = $scope.budget.name;
+         var name = $scope.budget.name;
+         var event = $scope.budget.event;
+         var amount = $scope.budget.amount;*/
+
+        $scope.master = {};
+
+        $scope.update = function(budget) {
+            $scope.master = angular.copy(budget);
+        };
+
+        $scope.reset = function() {
+            $scope.budget = angular.copy($scope.master);
+        };
+
+        $scope.reset();
+
+    });
+
+/*
+ "use strict";
+ angular.module('search-service', []).factory('searchService', function ($http) {
+ var service = {};
+ service.search = function (filterValue) {
+ return $http.get("api/ucds?filterValue=" + filterValue);
+ }
+ return service;
+ });
+ */
