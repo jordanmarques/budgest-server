b6ee7b880ecdb8b72edf76f5f606e80f2cb17cd6
salfai <salfai.fc@gmail.com>
2016-04-16 12:07:31 +0200

    new : add events and see events

--- src/main/resources/static/scripts/controllers/events.js
+++ src/main/resources/static/scripts/controllers/events.js
@@ -8,10 +8,34 @@
  * Controller of the budGestApp
  */
 angular.module('budGestApp')
-  .controller('EventsCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+  .controller('EventsCtrl', function ($scope, $http, person) {
+
+      //Ajout de l event
+      person.success(function (data) {
+          $scope.persons = data;
+      });
+
+      $scope.addEvent = function (event) {
+
+          $http.post('event/', event)
+              .success(function () {
+                  alert('Une e');
+              })
+              .error(function (data) {
+                  alert('Une erreur est survenue lors de la récupération de l\'utilisateur: ' + data.message);
+              });
+      };
+
+
+      //Affichage des event
+      $http.get('event')
+          .success(function (data) {
+              $scope.data_error = true;
+              $scope.data_success = false;
+              $scope.events = data;
+          })
+          .error(function () {
+              $scope.data_error = false;
+              $scope.data_success = true;
+          });
   });
